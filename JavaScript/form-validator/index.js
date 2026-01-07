a32b0f8d6e6ed44a9f9d86054138462990678eab
Mohitbyte29 <mohitupadhyay1025@gmail.com>
2026-01-07 21:35:25 +0530

    update form validator

--- JavaScript/form-validator/index.js
+++ JavaScript/form-validator/index.js
@@ -1,17 +1,14 @@
+
 const container = document.getElementsByClassName("container");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const cnfPassword = document.getElementById("cnf-password");
-const submitBtn = document.getElementById("submit-btn");
 const form = document.getElementById("validate-form");
 
+// ! nextElementSibling --> "give me the element right after this input"
+// * Optimized way
 
-
-function formValidate([isUsernameValid, isEmailValid, isPasswordValid, isCnfValid]){
-
-    return isEmailValid && isUsernameValid && isPasswordValid && isCnfValid;
-}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -20,7 +17,7 @@ form.addEventListener("submit", (e) => {
     const isPasswordValid = checkPassword();
     const isCnfValid = checkcnfPassword();
     
-    const result = formValidate([isUsernameValid, isEmailValid, isPasswordValid, isCnfValid]);
+    const result = isUsernameValid && isEmailValid && isPasswordValid && isCnfValid;
 
     if(result){
          form.submit();
@@ -32,49 +29,133 @@ form.addEventListener("submit", (e) => {
 
 })
 
+
+function showError(input, message){
+    input.classList.add("error");
+    input.classList.remove("success");
+    input.nextElementSibling.innerText = message;
+}
+
+function showSuccess(input){
+    input.classList.add("error");
+    input.classList.remove("success");
+    input.nextElementSibling.innerText = "";
+}
+
 function checkUsername(){
-    if(username.value.length < 6){
-        // alert("username length shoul be more than 6 ")
-        username.style.border = "2px solid red";
+    if(username.value.trim().length < 6){
+        showError(username, "Username length should be more than 6");
         return false;
     }
     else{
-        username.style.border = "2px solid green";
+        showSuccess(username);
         return true;
     }
 }
 
 function checkEmail(){
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!emailPattern.test(email.value)){
-        email.style.border = "2px solid red";
+    if(!emailPattern.test(email.value.trim())){
+        showError(email, "Enter the correct email");
         return false;
     }
     else{
-        email.style.border = "2px solid green";
+        showSuccess(email);
         return true;
     }
 }
 
 function checkPassword(){
-    if(password.value.length < 8){
-        email.style.border = "2px solid red";
+    if(password.value.trim().length < 8){
+        showError(password, "Password length should be more than 8");
         return false;
     }
     else{
-        email.style.border = "2px solid green";
+        showSuccess(password);
         return true;
     }
 }
 
 function checkcnfPassword(){
-    if(cnfPassword.value !== password.value){
-        cnfPassword.style.border = "2px solid red";
+    if(cnfPassword.value.trim() !== password.value){
+        showError(cnfPassword, "Confirm password should be same as password");
         return false;
     }
     else{
-        cnfPassword.style.border = "2px solid green";
+        showSuccess(cnfPassword);
         return true;
     }
 }
+
+
+
+// ! My way
+// function formValidate([isUsernameValid, isEmailValid, isPasswordValid, isCnfValid]){
+
+//     return isEmailValid && isUsernameValid && isPasswordValid && isCnfValid;
+// }
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const isUsernameValid = checkUsername();
+//     const isEmailValid = checkEmail();
+//     const isPasswordValid = checkPassword();
+//     const isCnfValid = checkcnfPassword();
+    
+//     const result = formValidate([isUsernameValid, isEmailValid, isPasswordValid, isCnfValid]);
+
+//     if(result){
+//          form.submit();
+//     } else{
+//         console.log("form is invalid");
+//     }
+
+//     console.log({ isUsernameValid, isEmailValid, isPasswordValid, isCnfValid });
+
+// })
+
+// function checkUsername(){
+//     if(username.value.length < 6){
+//         username.style.border = "2px solid red";
+//         return false;
+//     }
+//     else{
+//         username.style.border = "2px solid green";
+//         return true;
+//     }
+// }
+
+// function checkEmail(){
+//     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     if(!emailPattern.test(email.value)){
+//         email.style.border = "2px solid red";
+//         return false;
+//     }
+//     else{
+//         email.style.border = "2px solid green";
+//         return true;
+//     }
+// }
+
+// function checkPassword(){
+//     if(password.value.length < 8){
+//         email.style.border = "2px solid red";
+//         return false;
+//     }
+//     else{
+//         email.style.border = "2px solid green";
+//         return true;
+//     }
+// }
+
+// function checkcnfPassword(){
+//     if(cnfPassword.value !== password.value){
+//         cnfPassword.style.border = "2px solid red";
+//         return false;
+//     }
+//     else{
+//         cnfPassword.style.border = "2px solid green";
+//         return true;
+//     }
+// }
 
