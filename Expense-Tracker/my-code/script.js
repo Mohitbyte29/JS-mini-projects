140c1853d1ca3346ea8815560c84c902cacbbbdf
Mohitbyte29 <mohitupadhyay1025@gmail.com>
2026-01-10 00:01:42 +0530

    update

--- Expense-Tracker/my-code/script.js
+++
@@ -1,9 +0,0 @@
-const balance = document.getElementById('balance');
-const income = document.getElementById('income-amount');
-const expenses = document.getElementById('expense-amount');
-const transactionListEl = document.getElementById('transaction-list')
-const amount = document.getElementById('amount')
-const description = document.getElementById('description')
-const transactionFormEl = document.getElementById("transaction-form");
-
-
